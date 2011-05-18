7e649efc46e2437fb9cae8700ef36f1e02a5063f
Lotte <lotte@zombietetris.de>
2011-05-19 00:59:50 +0200

    moar refactoring

--- PyJama.py
+++ PyJama.py
@@ -18,7 +18,11 @@ class PyJama:
     # (yess, will fix the latter.)
 
     def __init__(self):
+        self.create_window()
+        self.load_config()
+        self.show_window()
 
+    def create_window(self):
         #create window
         self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
         
@@ -51,19 +55,45 @@ class PyJama:
         l_space = gtk.Label(None)
 
         #creating the buttons (duh)
-        button_show = gtk.Button("show crontab")
+        #button_show = gtk.Button("show crontab")
         button_scp = gtk.Button("Set Alarm")
         # !! idee: "launcher" methode, die das vospiel fuer beide macht und je nach uebergebenem parameter 
         # dann koper- oder anzeigemethode aufruft. -> TODO: connections neu
         # & klaeren: muss das self. sein?
-        button_show.connect("clicked", self.launcher,"show")
-        button_scp.connect("clicked",self.launcher,"copy")
+        #button_show.connect("clicked", self.launcher,"show")
+        button_scp.connect("clicked",self.launcher, "copy")
         
         #killswitch
         self.window.connect("destroy", self.destroy)
         #self.window.connect("delete_event". self.delete_event)'
         
-        #loading the configs
+                # add & show geraffel
+        self.ipbox.add(l_ip)
+        self.ipbox.add(self.e_ip)
+        for i in range(0,7):
+            self.daybox.add(self.days[i])
+        labelbox.add(l_mon)
+        labelbox.add(l_tue)
+        labelbox.add(l_wed)
+        labelbox.add(l_thu)
+        labelbox.add(l_fri)
+        labelbox.add(l_sat)
+        labelbox.add(l_sun)
+        labelbox.add(l_space)
+        #buttonbox.add(button_show)
+        buttonbox.add(button_scp)
+        self.timebox.add(labelbox)
+        self.timebox.add(self.daybox)
+        self.allbox.add(self.ipbox)
+        self.allbox.add(self.timebox)
+        self.allbox.add(buttonbox)
+        self.window.add(self.allbox)
+
+    def show_window(self):
+        self.window.show_all()
+    
+    def load_config(self):
+        #loading the configs to the window's panels
         self.config=ConfigParser.ConfigParser()
         self.pyjamapath = os.path.expanduser('~')+'/.config/PyJama/'
         print self.pyjamapath
@@ -81,40 +111,13 @@ class PyJama:
                 pyjamafile.close()
         else: 
             os.makedirs(self.pyjamapath)
-            
-        
-        # add & show geraffel
-        self.ipbox.add(l_ip)
-        self.ipbox.add(self.e_ip)
-        for i in range(0,7):
-            self.daybox.add(self.days[i])
-        labelbox.add(l_mon)
-        labelbox.add(l_tue)
-        labelbox.add(l_wed)
-        labelbox.add(l_thu)
-        labelbox.add(l_fri)
-        labelbox.add(l_sat)
-        labelbox.add(l_sun)
-        labelbox.add(l_space)
-        buttonbox.add(button_show)
-        buttonbox.add(button_scp)
-        self.timebox.add(labelbox)
-        self.timebox.add(self.daybox)
-        self.allbox.add(self.ipbox)
-        self.allbox.add(self.timebox)
-        self.allbox.add(buttonbox)
-        self.window.add(self.allbox)
-        self.window.show_all()
 
 
-    def launcher(self, widget,button_action):
+    def launcher(self, widget, button_action):
+        # will keep the button_action thingamabob in case there are other buttons in the future
         self.store_settings()
-        if button_action is "show":
-            buff = self.generate_ctab(widget, "show")
-            self.ctabpop(buff)
-        elif button_action is "copy":
-            self.copy_ctab()
-            self.generate_ctab(widget,"copy")
+        if button_action is "copy":
+            self.copy_ctab(widget)
 
 
     def store_settings(self):
@@ -127,7 +130,9 @@ class PyJama:
         if not self.config.has_section('days'):
             self.config.add_section('days')
         for i in range(0,7):
-            self.config.set('days',str(i),self.days[i].get_text())
+            day_str =self.days[i].get_text()
+            # TODO: don't store the non-digit chars
+            self.config.set('days',str(i),day_str)
 
         # store ip in config
         if not self.config.has_section('IP'):
@@ -143,60 +148,61 @@ class PyJama:
         pyjamafile.close()
         
         
-    def generate_ctab(self, widget, button_action):
+    def generate_ctab(self):
         """ generates the crontab (duh!), writes it into the config dir (lazy way
-        of implementing scp, will eventually change that) """
+        of implementing the copy operation, will eventually change that) """
 
         # first of all, we parse the entries so they will fit into a crontab
-        times = self.parse_times()
+        times, nondigit_error = self.parse_times()
         
         # doppelt gemoppelt, weiss aber nicht wie ich das sonst abfragensparsamer 
         # implementieren soll.
-        
-        # pipe crontab to buffer (for popup) :
-        if button_action is "show":
             
-            # creating local the crontab so it can be scp'd. this is only temporary and
-            # yes, this is idiotic. But I really need something to work right now.
-            # TODO: fix this mess.
+        # creating local the crontab so it can be scp'd. this is only temporary and
+        # yes, this is idiotic. But I really need something to work right now.
+        # TODO: fix this mess.
 
-            pyjamatab= open(self.pyjamapath+'crontab', 'w')      
+        pyjamatab= open(self.pyjamapath+'crontab', 'w')      
             
-            for i in range (0,7):
-                weekday=times[i]
-                for j in weekday:
-                    line = "& "+j+" * * "+ str(i+1) +" sh /flash2/alarm-clock \n"
-                    pyjamatab.write(line)
-            pyjamatab.close()
-            #return --- <-- no return value needed here. Python, I love you for allowing me to do this.
+        for i in range (0,7):
+            weekday=times[i]
+            for j in weekday:
+                line = "& "+j+" * * "+ str(i+1) +" sh /flash2/alarm-clock \n"
+                pyjamatab.write(line)
+        pyjamatab.close()
         
-        
-        # store crontab in pyjamatab: 
-        if button_action is "show":
+        return nondigit_error
+        # pipe crontab to buffer (for popup) (deprecated, but just in case..) 
+        """if button_action is "show":
             buff = gtk.TextBuffer(table=None)       
             for i in range (0,7):
                 weekday=times[i]
                 for j in weekday:
                     line = "& "+j+" * * "+ str(i+1) +" sh /flash2/alarm-clock \n"
                     buff.insert(buff.get_end_iter(), line)
-            return buff
+            return buff"""
         
         
-    def copy_ctab(self):
+    def copy_ctab(self, widget):
         """ copies the generated ctab (stored @ pyjamapath)
         to the VIP behind the specified IP"""
+        nondigit_err = self.generate_ctab()
         
         sys =os.system('scp "%s" "%s:%s"' % (self.pyjamapath+'crontab', 'root@'+self.e_ip.get_text(),'/flash2/crontab') )
         
         #check if the scp was successful and display an error message if not
         # TODO: watch stdin/stderr for password requests etc (for example if somebody set a root pswd)
         err_msg=gtk.TextBuffer(table=None)
+        err_msg.set_text("we've got news for you: \n ===================================================== \n")
+        if nondigit_err:
+            print "nondigiterr"
+            err_msg.insert_at_cursor("\n -* It looks like you had some non-digit characters in your alarm times. \n You may want to double-check them. \n We still updated all the correct times, though :) \n")
         if sys is not 0:
             #some error occured
-            err_msg.set_text("\n o noes! It looks like setting the alarm has failed.\n Please check your settings and try again.")
+            err_msg.insert_at_cursor("\n -* o noes! It looks like setting the alarm has failed, probably because your Box didn't respond. \n Please check your settings and try again.")
             self.event_popup(err_msg)
         else:
-            err_msg.set_text("\n wheee! Setting the alarm was successful. Sleep tight!")
+            err_msg.insert_at_cursor("\n -* wheee! Setting the alarm was successful. Sleep tight!")
             self.event_popup(err_msg)
         
     def ctabpop(self, buff):
@@ -225,10 +231,10 @@ class PyJama:
             for j in lst:
                 tmp= j.split(":")
                 for k in range(0,len(tmp)):
-                    #check for non-digit chars
                     tmp[k] = tmp[k].strip()
                     #check for non-digit chars
-                    if not tmp[k].isdigit():
+                    if not tmp[k].isdigit() and tmp[k] != "":
+                        print "nondigit: "+str(tmp[k])
                         tmp[k]=""
                         nondigit=True
                 if len(tmp)==1: tmp.append("00")
@@ -236,11 +242,8 @@ class PyJama:
                 tmp = " ".join(tmp)
                 correct_times.append(tmp)
             new_days.append(correct_times)
-                    #if there have been non-digit chars, note the user
-        err = gtk.TextBuffer(table=None)
-        err.set_text("\n It looks like you had some non-digit characters in your alarm times. \n You may want to double-check them. \n We still updated all the correct times, though :)")
-        self.event_popup(err)
-        return new_days
+        #if there have been non-digit chars, notify the user
+        return new_days, nondigit
 
     def event_popup(self, msg):
         window = gtk.Window(gtk.WINDOW_TOPLEVEL)
